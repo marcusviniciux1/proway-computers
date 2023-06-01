c9772b247a58335df7bb4445d03c5dab3c9b26b4
Marcus Vinicius <marcusvinicius.rodrigues@estudante.ufjf.br>
2023-06-01 19:45:15 -0300

    implementa a lógica da página de detalhes dos produtos

--- src/app/produtos/detalhes-produto/detalhes-produto.component.ts
+++ src/app/produtos/detalhes-produto/detalhes-produto.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduto } from 'src/app/produtos';
+import { ProdutosService } from 'src/app/produtos.service';
 
 @Component({
   selector: 'app-detalhes-produto',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./detalhes-produto.component.css']
 })
 export class DetalhesProdutoComponent {
+  produto: IProduto | undefined;
+  quantidade = 1;
 
+  constructor(
+    private produtoService: ProdutosService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const produtoId = Number(routeParams.get("id"));
+    this.produto = this.produtoService.getOne(produtoId);
+  }
 }
