031fe5c994ec1a3cd3b898396ffa0cbefd51a38f
Marcus Vinicius <marcusvinicius.rodrigues@estudante.ufjf.br>
2023-06-02 16:47:08 -0300

    adiciona lógica da implementação "Adicionar ao carrinho"

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarrinhoService } from '../carrinho.service';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor(
-  
+    public carrinhoService: CarrinhoService
   ) { }
 
   ngOnInit(): void {
